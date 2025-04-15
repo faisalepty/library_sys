0c9ad3fc830c62c40d020f5bdd91e434aba1cbe1
faisalepty <jojomadready@gmail.com>
2025-04-15 20:25:31 +0300

    staff auth

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@e^mm6m0h#vdrtw)o6mu9ad%_9l11k7fnd_812=&)q4sgdb5ha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://flibrary.pythonanywhere.com/']
 
 
 # Application definition
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'library.urls'
@@ -126,6 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
